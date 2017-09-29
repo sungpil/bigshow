0951f144e6b1543c4ad87f5b82d79999ad6249a8
sungpil <kanchi@sundaytoz.com>
2017-09-29 12:11:45 +0900

    refectoring gmail oauth2

--- com/google/gmailauth.py
+++ com/google/gmailauth.py
@@ -7,15 +7,13 @@ import time
 gmail_auth = Blueprint('gmail_auth', __name__, template_folder='templates')
 
 def oauth2check():
-    Logger.debug("oauth2check")
     if 'user' not in session:
-        Logger.debug("not credentials")
+        Logger.error("Credentials not exists")
         return redirect(__getFlow().step1_get_authorize_url())
     else:
-        Logger.debug("oauth2check: success")
-    return None
+        return None
 
-@gmail_auth.route('/gmail/oauth2callback')
+@gmail_auth.route('/oauth2/callback')
 def oauth2callback():
     code = request.args['code']
     Logger.debug("oauth2callback code={code}".format(code=code))
@@ -23,16 +21,19 @@ def oauth2callback():
     email = credentials.id_token[u'email']
     user = Admin().get(email)
     if user:
-        Logger.debug(
-            "code = {code}, user_id={user_id}, email={email}".format(code=code, user_id=user['idx'], email=email))
+        Logger.debug("user_id={user_id}, email={email}".format(user_id=user['idx'], email=email))
         session['user'] = {'id':user['idx'],'time':time.time()}
         return redirect(url_for('chart'))
     else:
-        abort(401)
+        return redirect(url_for('gmail_auth.fail'))
+
+@gmail_auth.route('/oauth2/fail')
+def fail():
+    abort(401)
 
 def __getFlow():
     from config.dev import config
     return OAuth2WebServerFlow(client_id=config['google']['client_id'],
                                client_secret=config['google']['client_secret'],
                                scope='https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile',
-                               redirect_uri=url_for('gmail_auth.oauth2callback', _external=True))
+                               redirect_uri=url_for('gmail_auth.oauth2callback', _external=True).replace('127.0.0.1','localhost'))
