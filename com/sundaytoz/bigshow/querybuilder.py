355e7beb62d2a9432a1d3f2acd57cb67ddda456b
sungpil <kanchi@sundaytoz.com>
2017-11-10 17:33:10 +0900

    retention graph modified

--- com/sundaytoz/bigshow/querybuilder.py
+++ com/sundaytoz/bigshow/querybuilder.py
@@ -14,16 +14,19 @@ class QueryBuilder:
         if QUERY_TYPE.CUSTOM == int(query_type):
             option = json.loads(query)
             date_range = option['range']
-            interval = option['interval']
-            region = option['region']
+            interval = date_range-1
+            region = option['region'] if 'region' in option else None
+            platform = QueryBuilder._getPlatform(option['platform']) if 'platform' in option else None
             end_date = datetime.datetime.now() - datetime.timedelta(days=1)
             start_date = end_date - datetime.timedelta(days=interval)
-            return QueryBuilder.retention(start_date, end_date, date_range, region)
+            query = QueryBuilder.retention(start_date, end_date, date_range, region, platform)
+            Logger().debug(query)
+            return query
         else:
             return query
 
     @staticmethod
-    def retention(start_date, end_date, interval, region):
+    def retention(start_date, end_date, interval, region, platform):
         project_id = config['bigquery']['project_id']
         dataset_nru = "{0}.{1}".format(project_id, config['bigquery']['dataset']['nru'])
         dataset_dau = "{0}.{1}".format(project_id, config['bigquery']['dataset']['dau'])
@@ -32,8 +35,11 @@ class QueryBuilder:
             end_date = today
         if start_date > end_date:
             start_date = end_date
+        where_nru = QueryBuilder._getWhereRegionPlatform(region=region, platform=platform, table_alias=None)
+        where_dru = QueryBuilder._getWhereRegionPlatform(region=region, platform=platform, table_alias='t1')
         unions = []
         Logger().debug("retention: start_date={0}, end_date={1}".format(start_date, end_date))
+        # make union
         while start_date <= end_date:
             target_date_list = [start_date + datetime.timedelta(days=x) for x in range(0, interval)]
             cnt = 0
@@ -43,11 +49,14 @@ class QueryBuilder:
                 if target_date <= today:
                     if cnt == 0:
                         date_str = target_date.strftime('%y%m%d')
-                        query_list.append("(SELECT count(*) FROM `{0}.{1}` WHERE region = '{2}') r{3} ".format(dataset_nru, date_str, region, cnt))
+                        query_list.append(
+                            "(SELECT count(*) FROM `{dataset_nru}.{date_str}` {where}) r{cnt} ".format(
+                                dataset_nru=dataset_nru, date_str=date_str, cnt=cnt, where=where_nru))
                     else:
                         query_list.append(
-                            "(SELECT count(*) FROM `{0}.{1}` t1 inner join `{2}.{3}` t2 ON t1.resettable_device_id = t2.resettable_device_id AND t1.region = t2.region AND t1.region = '{4}') r{5}".format(
-                                dataset_nru, date_str, dataset_dau, target_date.strftime('%y%m%d'), region, cnt))
+                            "(SELECT count(*) FROM `{dataset_nru}.{date_str}` t1 inner join `{dataset_dau}.{dau_date_str}` t2 ON t1.uid = t2.uid AND t1.region = t2.region {where}) r{cnt}".format(
+                                dataset_nru=dataset_nru, date_str=date_str, dataset_dau=dataset_dau,
+                                dau_date_str=target_date.strftime('%y%m%d'), cnt=cnt, where=where_dru))
                 else:
                     query_list.append("0 r{0}".format(cnt))
                 cnt += 1
@@ -58,3 +67,34 @@ class QueryBuilder:
             return "SELECT * FROM {0} ORDER BY dt".format(" union all ".join(unions))
         else:
             return None
+
+    @staticmethod
+    def _getInterval(range, interval):
+        if interval is None:
+            interval = range-1
+        return max(range-1, interval)
+
+    @staticmethod
+    def _getPlatform(platform):
+        if platform.lower() in ['android', 'a', 'aos', 'google']:
+            return 'A'
+        elif platform.lower() in ['apple', 'i', 'ios']:
+            return 'I'
+        else:
+            return None
+
+    @staticmethod
+    def _getWhereRegionPlatform(region, platform, table_alias):
+        where_list = []
+        if table_alias is not None:
+            table_alias += '.'
+        else:
+            table_alias = ''
+        if region is not None:
+            where_list.append("{table_alias}region='{region}'".format(table_alias=table_alias,region=region))
+        if platform.upper() is not None:
+            where_list.append("{table_alias}platform='{platform}'".format(table_alias=table_alias,platform=platform))
+        if len(where_list) > 0:
+            return "WHERE {where_list}".format(where_list=' AND '.join(where_list))
+        else:
+            return ''
--- static/js/stzchart.js
+++ static/js/stzchart.js
@@ -10,7 +10,7 @@ sundaytoz.chart.RetentionTable = class RetentionTable {
 		var table_tpl = '<div class="stz-graph-retention" style="width:100%; height:{height}; overflow:auto"><table class="stz-graph-retention-table">{header}{header-avg}{data-rows}</table></div>';
 		var height = (option.height.toString().includes('%')) ? option.height : option.height + 'px';
 		var header = this.makeHeader(data.shift());
-		var rttAndAvg = this.makeRttAndAvg(data);
+		var rttAndAvg = this.makeRttAndAvg(data, option);
 		var table = table_tpl.replace('{header}',header)
 				.replace('{header-avg}',this.makeHeader(rttAndAvg.avg))
 				.replace('{data-rows}',this.makeDataRow(rttAndAvg.rtt)).replace('{height}',height);
@@ -30,7 +30,7 @@ sundaytoz.chart.RetentionTable = class RetentionTable {
 		data.forEach(function(row) {
 			var td_list = [];
 			var date = row[0];
-			td_list.push('<td class="col-date">'+date+'<br/>'+row[1].value+'</td>');
+			td_list.push('<td class="col-date"><span class="txt-date">'+date+'</span><br/><span class="txt-nru-value">'+row[1].value+'</span></td>');
 			for(var i=1,len=row.length;i<len;i++) {
 				var rtt = row[i].rtt;
 				var data_str = (rtt > 0) ? rtt+'%' : '&nbsp;';
@@ -54,24 +54,23 @@ sundaytoz.chart.RetentionTable = class RetentionTable {
 		});
 		return tr_list.join('');
 	}
-	makeRttAndAvg(data) {
+	makeRttAndAvg(data, option) {
 		var rtt_list = [];
 		var rtt_sum_list= [];
 		var avg = ['','100%'];
-		var len = data[0].length-2;
-		while(len-- > 0) {
-			rtt_sum_list.push([]);
-		}
 		data.forEach(function(row){
 			var result_row = [];
 			var nru = row[1];
 			result_row.push(row[0]);
 			result_row.push({'value':nru,'rtt':'100'});
-			for(var i=2; i<row.length; i++) {
-				var dru = row[i];
-				if(dru > 0) {
-					var rtt = dru/nru*100
-					rtt_sum_list[i-2].push(rtt);
+			for(var i=2, dau_idx=0; i<row.length; i++,dau_idx++) {
+				var dau = row[i];
+				if(dau > 0) {
+					var rtt = dau/nru*100
+					if(rtt_sum_list.length < dau_idx+1) {
+					    rtt_sum_list[dau_idx] = [];
+					}
+					rtt_sum_list[dau_idx].push(rtt);
 					result_row.push({'value':row[i],'rtt':rtt.toFixed(1)});
 				} else {
 					result_row.push({'value':'','rtt':''});
